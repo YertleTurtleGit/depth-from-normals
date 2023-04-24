f88d9177608a0ac34dced829b1374339be89120d
YertleTurtleGit <github@galzone.de>
2023-04-24 21:26:10 +0200

    minor readability and performance optimizations

--- height_map.py
+++ height_map.py
@@ -1,4 +1,7 @@
 import numpy as np
+import cv2 as cv
+from multiprocessing.pool import ThreadPool as Pool
+from multiprocessing import cpu_count
 from math import pi, cos, sin, radians
 from typing import Tuple, List, Union
 import numba
@@ -28,7 +31,7 @@ def integrate_gradient_field(
 ) -> np.ndarray:
     heights = np.zeros(gradient_field.shape)
 
-    for d1 in range(heights.shape[1 - axis]):
+    for d1 in numba.prange(heights.shape[1 - axis]):
         sum_value = 0
         for d2 in range(heights.shape[axis]):
             coordinates = (d1, d2) if axis == 1 else (d2, d1)
@@ -61,63 +64,36 @@ def combine_heights(*heights: np.ndarray) -> np.ndarray:
 
 
 def rotate(matrix: np.ndarray, angle: float) -> np.ndarray:
-    # TODO Simplify
-    import cv2 as cv
+    h, w = matrix.shape[:2]
+    center = (w / 2, h / 2)
 
-    image_size = (matrix.shape[1], matrix.shape[0])
-    image_center = tuple(np.array(image_size) / 2)
+    rotation_matrix = cv.getRotationMatrix2D(center, angle, 1.0)
+    corners = cv.transform(
+        np.array([[[0, 0], [w, 0], [w, h], [0, h]]]), rotation_matrix
+    )[0]
 
-    rotation_matrix = np.matrix(
-        np.vstack([cv.getRotationMatrix2D(image_center, angle, 1.0), [0, 0, 1]])
-    )
-    translation_matrix = np.matrix(np.identity(3))
+    _, _, w, h = cv.boundingRect(corners)
 
-    w2 = image_size[0] * 0.5
-    h2 = image_size[1] * 0.5
-
-    rot_mat_notranslate = np.matrix(rotation_matrix[0:2, 0:2])
-
-    tl = (np.array([-w2, h2]) * rot_mat_notranslate).A[0]
-    tr = (np.array([w2, h2]) * rot_mat_notranslate).A[0]
-    bl = (np.array([-w2, -h2]) * rot_mat_notranslate).A[0]
-    br = (np.array([w2, -h2]) * rot_mat_notranslate).A[0]
-
-    x_coords = [pt[0] for pt in [tl, tr, bl, br]]
-    x_pos = [x for x in x_coords if x > 0]
-    x_neg = [x for x in x_coords if x < 0]
-
-    y_coords = [pt[1] for pt in [tl, tr, bl, br]]
-    y_pos = [y for y in y_coords if y > 0]
-    y_neg = [y for y in y_coords if y < 0]
-
-    right_bound = max(x_pos)
-    left_bound = min(x_neg)
-    top_bound = max(y_pos)
-    bot_bound = min(y_neg)
-
-    new_w = int(abs(right_bound - left_bound))
-    new_h = int(abs(top_bound - bot_bound))
-    new_image_size = (new_w, new_h)
-
-    new_midx = new_w * 0.5
-    new_midy = new_h * 0.5
-
-    dx = int(new_midx - w2)
-    dy = int(new_midy - h2)
-
-    translation_matrix = np.matrix(np.array([[1, 0, dx], [0, 1, dy], [0, 0, 1]]))
-    affine_mat = (np.matrix(translation_matrix) * rotation_matrix)[0:2, :]
-    result = cv.warpAffine(matrix, affine_mat, new_image_size, flags=cv.INTER_LINEAR)
+    rotation_matrix[0, 2] += w / 2 - center[0]
+    rotation_matrix[1, 2] += h / 2 - center[1]
+    result = cv.warpAffine(matrix, rotation_matrix, (w, h), flags=cv.INTER_LINEAR)
 
     return result
 
 
 def rotate_vector_field_normals(normals: np.ndarray, angle: float) -> np.ndarray:
-    rotated_normals = normals.copy()
-    rotated_normals[:, :, 0] = normals[:, :, 0] * cos(radians(angle))
-    rotated_normals[:, :, 0] -= normals[:, :, 1] * sin(radians(angle))
-    rotated_normals[:, :, 1] = normals[:, :, 0] * sin(radians(angle))
-    rotated_normals[:, :, 1] += normals[:, :, 1] * cos(radians(angle))
+    angle = np.radians(angle)
+    cos_angle = np.cos(angle)
+    sin_angle = np.sin(angle)
+
+    rotated_normals = np.empty_like(normals)
+    rotated_normals[:, :, 0] = (
+        normals[:, :, 0] * cos_angle - normals[:, :, 1] * sin_angle
+    )
+    rotated_normals[:, :, 1] = (
+        normals[:, :, 0] * sin_angle + normals[:, :, 1] * cos_angle
+    )
+
     return rotated_normals
 
 
@@ -140,7 +116,6 @@ def integrate_vector_field(
     target_iteration_count: int,
     thread_count: int,
 ) -> np.ndarray:
-    from multiprocessing.pool import ThreadPool as Pool
 
     shape = vector_field.shape[:2]
     angles = np.linspace(0, 90, target_iteration_count, endpoint=False)
@@ -195,7 +170,7 @@ def estimate_height_map(
     mask: Union[np.ndarray, None] = None,
     height_divisor: float = 1,
     target_iteration_count: int = 250,
-    thread_count: int = 4,
+    thread_count: int = cpu_count(),
     raw_values: bool = False,
 ) -> np.ndarray:
     if mask is None:
